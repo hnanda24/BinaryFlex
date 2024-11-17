490f9802e068cdc10c291e8e6b7b684184b17001
Hardik <hnanda005@gmail.com>
2024-11-17 17:20:44 +0530

    FirstCommit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Hello Brother how are you my friend</h1>
+    </div>
+  )
+}
+
+export default App
